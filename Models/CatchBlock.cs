19b4c9a01033cdd6d176c2386c1be6219c193b4e
Pablo Meca <90964186+pjmeca@users.noreply.github.com>
2024-01-28 00:10:59 +0100

    Move catch blocks logic to CatchBlock

---
+++ Models/CatchBlock.cs
@@ -0,0 +1,42 @@
+﻿namespace Fluent.TryCatch.Models;
+
+public class CatchBlock
+{
+    public Type Type { get; private set; }
+
+    public Action<Exception>? Action { get; private set; }
+    public bool IsAction => Action != null;
+
+    public Func<Exception, object>? Func { get; private set; }
+    public bool IsFunc => Func != null;
+
+    public Func<Exception, bool>? When { get; set; }
+
+    public CatchBlock(Type type)
+    {
+        Type = type;
+    }
+
+    public CatchBlock(Type type, Action<Exception> action) : this(type)
+    {
+        Action = action;
+    }
+
+    public CatchBlock(Type type, Func<Exception, object>? func) : this(type)
+    {
+        Func = func;
+    }
+
+    public T Invoke<T>(Exception ex)
+    {
+        if (IsFunc)
+        {
+            return (T)Func!.Invoke(ex);
+        }
+        else if (IsAction)
+        {
+            Action!.Invoke(ex);
+        }
+        return default;
+    }
+}
