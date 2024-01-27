9dd26e9b1456372b1f4c32ce53c00d2c222db9a9
Pablo Meca <90964186+pjmeca@users.noreply.github.com>
2024-01-28 00:10:15 +0100

    Add When definition

---
+++ IOperators/ICatched.cs
@@ -0,0 +1,6 @@
+﻿namespace Fluent.TryCatch.IOperators;
+
+public interface ICatched : ICatchabble
+{
+    public ICatchabble When(Func<Exception, bool> filter);
+}
---
+++ Operators/Catched.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using Fluent.TryCatch.IOperators;
+
+namespace Fluent.TryCatch.Operators;
+
+public class Catched : Catchabble, ICatched
+{
+    public Catched(ICatchabble catchabble)
+    {
+        var properties = typeof(Catchabble)
+            .GetProperties(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static)
+            .ToList();
+        properties.ForEach(pi =>
+        {
+            if (pi.CanWrite)
+                pi.SetValue(this, pi.GetValue(catchabble, null), null);
+        });
+    }
+
+    public ICatchabble When(Func<Exception, bool> filter)
+    {
+        return this;
+    }
+}
